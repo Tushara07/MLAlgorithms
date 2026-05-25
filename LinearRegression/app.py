66383e9cba0a562d00f17bb86a6a4d724aeb27c5
Tushara07 <tusharakatamreddy@gmail.com>
2026-05-25 15:35:18 +0530

    Update app.py

--- LinearRegression/app.py
+++ LinearRegression/app.py
@@ -12,9 +12,6 @@ import os
 
 current_dir = os.path.dirname(__file__)
 
-st.write("Current Directory:", current_dir)
-st.write("Files:", os.listdir(current_dir))
-
 csv_path = os.path.join(current_dir, "advertising.csv")
 
 data = pd.read_csv(csv_path)
